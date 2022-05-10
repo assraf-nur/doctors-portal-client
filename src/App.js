4efdf413afffe918f3840f8477d1aad390ff3d91
assraf-nur <assraf.nur@gmail.com>
2022-05-10 22:34:03 +0600

    add a banner and links in navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import About from './Components/Pages/About/About';
+import Appointment from './Components/Pages/Appointment/Appointment';
+import Contact from './Components/Pages/Contact/Contact';
+import Login from './Components/Pages/Login/Login';
+import Navbar from './Components/Pages/Navbar/Navbar';
+import Reviews from './Components/Pages/Reviews/Reviews';
 
 function App() {
   return (
     <div>
-      <h2>hello</h2>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/appoint' element={<Appointment></Appointment>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light"],
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "cupcake",
+    ],
   },
 }

a7045c7322ab3c0f1f8f669713a18f0c62a09223
assraf-nur <assraf.nur@gmail.com>
2022-05-12 12:25:13 +0600

    implement login and signup with full of security and also implement require auth

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Home from './Components/Home/Home';
 import About from './Components/Pages/About/About';
 import Appointment from './Components/Pages/Appointment/Appointment';
 import Contact from './Components/Pages/Contact/Contact';
+import RequireAuth from './Components/Pages/Login/GetAuth';
 import Login from './Components/Pages/Login/Login';
+import SignUp from './Components/Pages/Login/SignUp';
 import Navbar from './Components/Pages/Navbar/Navbar';
 import Reviews from './Components/Pages/Reviews/Reviews';
 
@@ -16,10 +18,15 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
-        <Route path='/appoint' element={<Appointment></Appointment>}></Route>
+        <Route path='/appoint' element={
+          <RequireAuth>
+            <Appointment></Appointment>
+          </RequireAuth>
+        }></Route>
         <Route path='/reviews' element={<Reviews></Reviews>}></Route>
         <Route path='/contact' element={<Contact></Contact>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
       </Routes>
     </div>
   );

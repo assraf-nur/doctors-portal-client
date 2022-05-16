ed9e583f380704943413432298c5fdebf1bb2140
assraf-nur <assraf.nur@gmail.com>
2022-05-17 01:42:13 +0600

    add doctor form added

--- src/Components/Pages/Dashboard/AddDoctor.js
+++ src/Components/Pages/Dashboard/AddDoctor.js
@@ -1,11 +1,143 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useQuery } from "react-query";
+import Loading from "../Login/Loading";
 
 const AddDoctor = () => {
-    return (
-        <div>
-            <h2 className='text-3xl'>Add a doctor</h2>
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const { data: services, isLoading } = useQuery("services", () =>
+    fetch("http://localhost:5000/services").then((res) => res.json())
+  );
+
+  const onSubmit = async (data) => {
+    console.log("data", data);
+    //  navigate("/appoint");
+  };
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
+  return (
+    <div className="px-96 mt-5">
+      <h2 className="text-3xl">Add a doctor</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Name"
+            className="input input-bordered w-full max-w-xs"
+            {...register("name", {
+              required: {
+                value: true,
+                message: "Name is Required",
+              },
+            })}
+          />
+          <label className="label">
+            {errors.name?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.name.message}
+              </span>
+            )}
+          </label>
         </div>
-    );
+
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            placeholder="Email"
+            className="input input-bordered w-full max-w-xs"
+            {...register("email", {
+              required: {
+                value: true,
+                message: "Email is Required",
+              },
+              pattern: {
+                value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                message: "Provide a valid Email",
+              },
+            })}
+          />
+          <label className="label">
+            {errors.email?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.email.message}
+              </span>
+            )}
+            {errors.email?.type === "pattern" && (
+              <span className="label-text-alt text-red-500">
+                {errors.email.message}
+              </span>
+            )}
+          </label>
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Specialization</span>
+          </label>
+          <select {...register("specialty")} class="border select w-full max-w-xs">
+            {
+                services.map(service => <option key={service._id} value={service.name}>{service.name}</option>)
+            }
+          </select>
+          <label className="label">
+            {errors.password?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.password.message}
+              </span>
+            )}
+            {errors.password?.type === "minLength" && (
+              <span className="label-text-alt text-red-500">
+                {errors.password.message}
+              </span>
+            )}
+          </label>
+        </div>
+
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Image</span>
+          </label>
+          <input
+            type="file"
+            placeholder="Image"
+            className="input input-bordered w-full max-w-xs"
+            {...register("image", {
+              required: {
+                value: true,
+                message: "Image is Required",
+              },
+            })}
+          />
+          <label className="label">
+            {errors.name?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.name.message}
+              </span>
+            )}
+          </label>
+        </div>
+
+        <input
+          className="btn w-full max-w-xs text-white"
+          type="submit"
+          value="Add a doctor"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default AddDoctor;
